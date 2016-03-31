6f1be1d2569d70d020a3c0f344ff070173dc18fd
rubenabix <rubenabix@gmail.com>
2016-03-31 17:09:12 -0600

    clean code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -52,44 +52,6 @@
     autoScrolling:false,
     verticalCentered: true,
     fitToSection: false
-
-    // afterLoad: function(anchorLink, index) {
-    //   if (index == 5) {
-    //     $('#fp-nav').hide();
-    //   }
-    // },
-    //
-    // onLeave: function(index, nextIndex, direction) {
-    //   if(index == 5) {
-    //     $('#fp-nav').show();
-    //   }
-    // },
-    //
-    // afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-    //   if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    //     $.fn.fullpage.setAllowScrolling(false, 'up');
-    //     $header_top.css('background', 'transparent');
-    //     $nav.css('background', 'transparent');
-    //     $(this).css('background', '#374140');
-    //     $(this).find('h2').css('color', 'white');
-    //     $(this).find('h3').css('color', 'white');
-    //     $(this).find('p').css(
-    //       {
-    //         'color': '#DC3522',
-    //         'opacity': 1,
-    //         'transform': 'translateY(0)'
-    //       }
-    //     );
-    //   }
-    // },
-    //
-    // onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-    //   if(anchorLink == 'fifthSection' && slideIndex == 1) {
-    //     $.fn.fullpage.setAllowScrolling(true, 'up');
-    //     $header_top.css('background', 'rgba(0, 47, 77, .3)');
-    //     $nav.css('background', 'rgba(0, 47, 77, .25)');
-    //   }
-    // }
   });
 
 })(window.jQuery, window.Handlebars)
